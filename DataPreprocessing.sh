ba1a76a23b920482d984f7771daebf6b5fe933de
Parker Oakes <parker.oakes@clkmm.com>
2021-03-04 21:17:15 -0600

    updated DataPreprocessing to format raw data into csv

--- DataPreprocessing.sh
+++ DataPreprocessing.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Constructing CSV Format
+cat Scripture_Raw.txt |\
+	sed 's/ KJV\] /",/g' |\
+	sed 's/\[/"/' |\
+	sed 's/\s/","/' |\
+	sed 's/\:/","/' |\
+	sed -e '1i"Book","Chapter","Verse","Text"\'
+
+# Constructing Scripture Text Only CSV Format
+
+
+# Constructing Strongs Numbers only CSV Format
+
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Word Study on Biblical Repentance
 
+1. raw
+2. scriptures
+3. strongs
 
