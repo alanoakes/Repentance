27c19877a49ae97cf17fa1a565cc8a19df9b3af4
Parker Oakes <parker.oakes@clkmm.com>
2021-03-04 22:55:20 -0600

    cleaned text for strongs numbers only with Scripture_Strongs.csv

--- DataPreprocessing.sh
+++ DataPreprocessing.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Constructing CSV Format
-cat Scripture_Raw.txt |\
-	sed 's/ KJV\] /",/g' |\
-	sed 's/\[/"/' |\
-	sed 's/\s/","/' |\
-	sed 's/\:/","/' |\
-	sed -e '1i"Book","Chapter","Verse","Text"\' \
-	> Scripture_TextAndStrongs.csv
-
-# Constructing Scripture Text Only CSV Format
-cat Scripture_TextAndStrongs.csv |\
-	sed -e 's/\[[^][]*\]//g' \
-	> Scripture_Text.csv
+# cat Scripture_Raw.txt |\
+# 	sed 's/ KJV\] /",/g' |\
+# 	sed 's/\[/"/' |\
+# 	sed 's/\s/","/' |\
+# 	sed 's/\:/","/' |\
+# 	sed -e '1i"Book","Chapter","Verse","Text"\' \
+# 	> Scripture_TextAndStrongs.csv
+# 
+# # Constructing Scripture Text Only CSV Format
+# cat Scripture_TextAndStrongs.csv |\
+# 	sed -e 's/\[[^][]*\]//g' \
+# 	> Scripture_Text.csv
 
 
 # Constructing Strongs Numbers only CSV Format
-
+cat Scripture_TextAndStrongs.csv |\
+	sed 's/\(^\|]\)[^][]*\(\[\|$\)/\1\2/g' |\
+	sed 's/\]\[/,/g' |\
+	sed 's/\[/"/g' |\
+	sed 's/\]/"/g' \
+	> Scripture_Strongs.csv
 
