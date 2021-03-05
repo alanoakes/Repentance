59a00bc923b0d271798d4700982dc57e2633bbb5
Parker Oakes <parker.oakes@clkmm.com>
2021-03-04 21:55:22 -0600

    updated DataPreprocessing.sh for text only stdout = Scripture_Text.csv

--- DataPreprocessing.sh
+++ DataPreprocessing.sh
@@ -6,9 +6,13 @@ cat Scripture_Raw.txt |\
 	sed 's/\[/"/' |\
 	sed 's/\s/","/' |\
 	sed 's/\:/","/' |\
-	sed -e '1i"Book","Chapter","Verse","Text"\'
+	sed -e '1i"Book","Chapter","Verse","Text"\' \
+	> Scripture_TextAndStrongs.csv
 
 # Constructing Scripture Text Only CSV Format
+cat Scripture_TextAndStrongs.csv |\
+	sed -e 's/\[[^][]*\]//g' \
+	> Scripture_Text.csv
 
 
 # Constructing Strongs Numbers only CSV Format
