e82bb2aa3119f0721a5b1eee7feeac2069e2f993
Parker Oakes <parker.oakes@clkmm.com>
2021-03-04 23:43:43 -0600

    updated DataPreprocessing.sh for StrongsA and StrongsB to include verse refs

--- DataPreprocessing.sh
+++ DataPreprocessing.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Constructing CSV Format
-# cat Scripture_Raw.txt |\
-# 	sed 's/ KJV\] /",/g' |\
-# 	sed 's/\[/"/' |\
-# 	sed 's/\s/","/' |\
-# 	sed 's/\:/","/' |\
-# 	sed -e '1i"Book","Chapter","Verse","Text"\' \
-# 	> Scripture_TextAndStrongs.csv
-# 
-# # Constructing Scripture Text Only CSV Format
-# cat Scripture_TextAndStrongs.csv |\
-# 	sed -e 's/\[[^][]*\]//g' \
-# 	> Scripture_Text.csv
+cat Scripture_Raw.txt |\
+	sed 's/ KJV\] /",/g' |\
+	sed 's/\[/"/' |\
+	sed 's/\s/","/' |\
+	sed 's/\:/","/' |\
+	sed -e '1i"Book","Chapter","Verse","Text"\' \
+	> Scripture_TextAndStrongs.csv
+
+# Constructing Scripture Text Only CSV Format
+cat Scripture_TextAndStrongs.csv |\
+	sed -e 's/\[[^][]*\]//g' \
+	> Scripture_Text.csv
 
 
 # Constructing Strongs Numbers only CSV Format
@@ -20,6 +20,14 @@ cat Scripture_TextAndStrongs.csv |\
 	sed 's/\(^\|]\)[^][]*\(\[\|$\)/\1\2/g' |\
 	sed 's/\]\[/,/g' |\
 	sed 's/\[/"/g' |\
-	sed 's/\]/"/g' \
-	> Scripture_Strongs.csv
+	sed 's/\]/"/g' |\
+	sed -e '1d' |\
+	sed -e '1i"Strongs"\' \
+	> Scripture_StrongsA.csv
+
+# Concat scripture refs into strongs numbers
+cut -d "," -f 1-3 Scripture_TextAndStrongs.csv > Scripture_Refs.csv
+paste -d "," Scripture_Refs.csv Scripture_Strongs.csv |\
+	awk -F',' '{print $0}' \
+	> Scripture_StrongsB.csv
 
